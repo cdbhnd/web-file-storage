5483e8aa4ade4316760b2f0784bf2b7fab9abb66
urospajic <uros.pajic@codebehind.rs>
2018-11-29 13:08:36 +0100

    Bypass authorization of GET requests

--- authorization.js
+++ authorization.js
@@ -2,7 +2,7 @@ var config = require('./config.json');
 
 const authorizationMiddleware = (req, res, next) => {
     console.log(config);
-    if (!req.headers.authorization || req.headers.authorization !== 'API ' + config.api_key) {
+    if (req.method !== 'GET' && (!req.headers.authorization || req.headers.authorization !== 'API ' + config.api_key)) {
         res.sendStatus(401);
     }
     next();
