25a0095146b1e679f7cc47fb101abdda678ca1ca
urospajic <uros.pajic@codebehind.rs>
2019-12-02 22:08:03 +0100

    added image resize functionality

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var fs = require('fs');
 var express = require('express');
 var FileStore = require('express-file-store');
 var authorizationMiddleware = require('./authorization');
- 
+const url = require('url');
+const resize = require('./resize')
+
 // using file system
 var fileStore = FileStore('fs', {
   path: __dirname + '/uploads'
@@ -12,6 +13,40 @@ var app = express();
 
 app.use(authorizationMiddleware);
 
+app.get('*', (req, res) => {
+  const cleanUrl = url.parse(req.url).pathname;
+  if (req.query && req.query.resize) {
+    const widthString = req.query.width
+    const heightString = req.query.height
+    let format = req.query.format
+
+    // Parse to integer if possible
+    let width, height
+    if (widthString) {
+      width = parseInt(widthString)
+    }
+    if (heightString) {
+      height = parseInt(heightString)
+    }
+    // Set the content-type of the response
+    if (!format) {
+      format = cleanUrl.indexOf('jpg') !== -1 ? 'jpg' : 'png';
+    }
+    res.type(`image/${format}`)
+
+    // Get the resized image
+    return resize(`${__dirname}/uploads${cleanUrl}`, format, width, height).pipe(res)
+  } else {
+    return fileStore.get(cleanUrl, function (err, file) {
+      if (file) {
+        res.sendFile(file.stream.path);
+      } else {
+        res.status(404).send('Not found');
+      }
+    });
+  }
+});
+
 app.use(fileStore.routes);
 
-app.listen(4200);
+app.listen(4222);
