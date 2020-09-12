4ee5de95895b75238ef5b3aa7813828ead9e5c77
urospajic <uros.pajic@codebehind.rs>
2020-09-12 14:38:41 +0300

    small adjustment in authorzation

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var app = express();
 app.use(authorizationMiddleware);
 
 app.get('*', (req, res) => {
-  const cleanUrl = url.parse(req.url).pathname;
+  const cleanUrl = decodeURIComponent(url.parse(req.url).pathname);
   if (req.query && req.query.resize) {
     const widthString = req.query.width
     const heightString = req.query.height
@@ -49,4 +49,4 @@ app.get('*', (req, res) => {
 
 app.use(fileStore.routes);
 
-app.listen(4222);
+app.listen(4200);
